2688622a36b882a5a504e6ef95745e5e45105c69
Sheep903 <s.l.giacomel@gmail.com>
2021-08-12 20:13:48 +0800

    Week 3
    Final Week 3, Thursday night update.

--- gCodeConverter.py
+++ gCodeConverter.py
@@ -31,16 +31,19 @@ def get_obj_data(objData):
             raise KeyError(str(err))
     return objDataDict
 
-def create_shape(shapeName, shapeDataDict):
+def create_shape(shapeName, shapeDataDict, gData):
     """Creates shape objects"""
     # Note: some objects may not exist
     # Case for each object
     if shapeName == "rect":
         newShape = Rectangle()
+        for item in gData:
+            newShape.add(item, gData[item])
         for item in shapeDataDict:
-            try:
-                newShape.add(item, shapeDataDict[item])
-            print(item + ": " + shapeDataDict[item])
+            newShape.add(item, shapeDataDict[item])
+    # Rince and repeate for each attribute
+    # Need objects for attributs
+    return newShape
 
 
 # Constants
@@ -72,24 +75,23 @@ for item in fileListStrip:
             # Create shape object
             print("Creating object: " + objName)
             objDict = get_obj_data(item)
-            shapeObjList = create_shape(objName, objDict)
-                    
-            
-            #Attribute(objName, )
-            #shapeObjList.append(object)
+            shapeObj = create_shape(objName, objDict, gData)
+            shapeObjList.append(shapeObj)
         elif objName in FORMAT_SYSTAX:
             print(objName + " format")
         elif objName == "g":
             # Creates g container
             print("Open g container")
-            objGData = get_obj_data(item)
-            print(objGData)
-            
-
+            gData = get_obj_data(item)
+        elif objName == "/g>":
+            # Close g container
+            print("Close g container")
+            gData = {}
         elif "/" in objName:
             print("Closing: " + str(objName[1:-1]))
         else:
             print("What dis: " + str(objName))
 
 # Print out instrutions
-# Nothing to see here (yet)
+for item in shapeObjList:
+    print(item)
