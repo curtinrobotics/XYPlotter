7528670d22bdc60af3f9345fe1aae18fe96d2e6e
uni-ro <thomas.unipan@student.curtin.edu.au>
2024-02-28 16:03:52 +0800

    Updated log file to work with gui

--- main/log.py
+++ main/log.py
@@ -3,6 +3,10 @@ log.py
 Class to log error/warning/error messages.
 """
 
+import datetime
+import constants as const
+import inputOutput as io
+
 # Variables
 log = None
 
@@ -12,6 +16,18 @@ class DebugLog():
     """Constructor create blank list to add to"""
     def __init__(self):
         self.dLog = []
+        self.dLog.append(self.__DebugLogEntry('timestamp', str(datetime.datetime.now())))
+        io.writeFileData(const.TEMP_LOG_PATH, str(self) + '\n')
+
+    def __str__(self):
+        
+        entries = []
+        
+        for i in range(len(self.dLog)):
+            #entry = self.dLog[i]
+            entries.append(self.dLog[i])#[entry.cat, entry.msg])
+
+        return '\n'.join([str(e) for e in entries])
 
     """Adding entry to log"""
     def newLog(self, cat, msg):
@@ -19,6 +35,7 @@ class DebugLog():
             self.newlog("error", "Category \"" + str(cat) + "\" does not exist for \"" + str(msg) + "\"")
         else:
             self.dLog.append(self.__DebugLogEntry(cat, msg))
+            self.updateLogFile(str(self.dLog[-1]) + '\n')
 
     """Print log to terminal and return string"""
     def outputLog(self, error=False, warning=False, debug=False, terminal=True):
@@ -33,6 +50,9 @@ class DebugLog():
         if terminal:
             print(outputString)
         return outputString
+    
+    def updateLogFile(self, data):
+        return io.appendFileData(const.TEMP_LOG_PATH, data)
 
     """Inner class for each entry"""
     class __DebugLogEntry():
@@ -40,6 +60,9 @@ class DebugLog():
         def __init__(self, cat, msg):
             self.cat = cat
             self.msg = msg
+
+        def __str__(self):
+            return f'{self.cat.upper()}:\t{self.msg}'
 
 """Log data variable"""
 def createLog():
