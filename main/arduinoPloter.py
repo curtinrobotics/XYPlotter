7ffdd0e8260bcc55ecb5648a363f4704319e1e86
theAsherMan <162021136+theAsherMan@users.noreply.github.com>
2024-03-07 20:27:28 +0800

    Update arduinoPloter.py

--- main/arduinoPloter.py
+++ main/arduinoPloter.py
@@ -15,9 +15,13 @@ in:
   pointList: instructions
     wrapper for a list of point objects
     points are instructions to raise pen, lower pen, or move pen to a given co-ord
-  
+
+  bool: ready
+    boolean value recieved from arduino to indicate readiness to recieve (more) instructions
 out:
 
-
-
+  int: x coord to move to.  -1 if raising pen, -2 if lowering
+  int: y coord to move to.  -1 if raising pen, -2 if lowering
 """
+
+
