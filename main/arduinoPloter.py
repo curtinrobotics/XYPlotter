7ce5ef02be28db708a14ea8dbc28b72cad530dfb
theAsherMan <162021136+theAsherMan@users.noreply.github.com>
2024-03-07 20:13:12 +0800

    Update arduinoPloter.py

--- main/arduinoPloter.py
+++ main/arduinoPloter.py
@@ -1,11 +1,21 @@
+import serial
+import pointList.py
+
+
 """
 
 handels comunication with arduino
 
 in:
 
+  string: the name of the port the arduino is on
+    supplyed by GUI
+    ** to be done automatically in future version **
 
-
+  pointList: instructions
+    wrapper for a list of point objects
+    points are instructions to raise pen, lower pen, or move pen to a given co-ord
+  
 out:
 
 
